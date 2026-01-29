9cbbb524445313a89ef424af5494afc006398e13
moro118 <mvillalobosortega@byupathway.edu>
2026-01-29 15:52:30 -0600

    update

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿﻿/*
  * CSE 212 Lesson 6C 
  * 
  * This code will analyze the NBA basketball data and create a table showing
@@ -27,10 +27,20 @@ public class Basketball
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            if (players.ContainsKey(playerId))
+                players[playerId] += points;
+            else
+                players[playerId] = points;
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        // Convert to array and sort by points (Value) descending
+        var sortedPlayers = players.ToArray();
+        Array.Sort(sortedPlayers, (p1, p2) => p2.Value - p1.Value);
 
-        var topPlayers = new string[10];
+        Console.WriteLine();
+        for (var i = 0; i < 10 && i < sortedPlayers.Length; ++i) {
+            Console.WriteLine($"{i + 1}. {sortedPlayers[i].Key} - {sortedPlayers[i].Value}");
+        }
     }
 }
--- week03/teach/UniqueLetters.cs
+++ week03/teach/UniqueLetters.cs
@@ -1,4 +1,4 @@
-﻿public static class UniqueLetters {
+﻿﻿public static class UniqueLetters {
     public static void Run() {
         var test1 = "abcdefghjiklmnopqrstuvwxyz"; // Expect True because all letters unique
         Console.WriteLine(AreUniqueLetters(test1));
@@ -14,13 +14,11 @@
     /// <param name="text">Text to check for duplicate letters</param>
     /// <returns>true if all letters are unique, otherwise false</returns>
     private static bool AreUniqueLetters(string text) {
-        // TODO Problem 1 - Replace the O(n^2) algorithm to use sets and O(n) efficiency
-        for (var i = 0; i < text.Length; ++i) {
-            for (var j = 0; j < text.Length; ++j) {
-                // Don't want to compare to yourself ... that will always result in a match
-                if (i != j && text[i] == text[j])
-                    return false;
-            }
+        var seen = new HashSet<char>();
+        foreach (var letter in text) {
+            // If Add returns false, the element was already in the set
+            if (!seen.Add(letter))
+                return false;
         }
 
         return true;
